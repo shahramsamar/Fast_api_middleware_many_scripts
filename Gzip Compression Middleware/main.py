2e9a4a08dd59f689e1c3c2892c2eb31084ae702b
shahramsamar <shahramsamar2010@gmail.com>
2024-10-23 14:40:02 +0330

    Example 4: Gzip Compression Middleware
    Example 4: Gzip Compression Middleware

---
+++ Gzip Compression Middleware/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.middleware.gzip import GZipMiddleware
+
+app = FastAPI()
+# Example 5: Gzip Compression Middleware
+# FastAPI has a built-in middleware for compressing responses using Gzip.
+# This middleware compresses responses larger than 1000 bytes using Gzip, which can help reduce network bandwidth usage.
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+
+@app.get("/")
+async def root():
+    return {"message": "This response will be compressed if it's bigger than 1000 bytes"}
