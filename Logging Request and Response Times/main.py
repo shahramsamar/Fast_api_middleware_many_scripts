6b0b31720e58477f08b2e0f61934ccc150f728fe
shahramsamar <shahramsamar2010@gmail.com>
2024-10-23 14:50:57 +0330

    Update main.py

--- Logging Request and Response Times/main.py
+++ Logging Request and Response Times/main.py
@@ -2,10 +2,10 @@ import time
 from fastapi import FastAPI, Request
 
 app = FastAPI()
-
-
-# Example 1: Logging Request and Response Times
-# This middleware logs the time taken to process each request and adds a custom header X-Process-Time to the response.
+"""
+Example : Logging Request and Response Times
+This middleware logs the time taken to process each request and adds a custom header X-Process-Time to the response.
+"""
 @app.middleware("http")
 async def log_request_time(request: Request, call_next):
     start_time = time.time()
