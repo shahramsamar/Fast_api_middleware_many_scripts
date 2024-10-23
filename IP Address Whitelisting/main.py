2e13dee22a474c01020f6dfc2be0099dd405073d
shahramsamar <shahramsamar2010@gmail.com>
2024-10-23 14:50:54 +0330

    Update main.py

--- IP Address Whitelisting/main.py
+++ IP Address Whitelisting/main.py
@@ -4,7 +4,8 @@ app = FastAPI()
 
 WHITELISTED_IPS = {"127.0.0.1", "192.168.1.1"}
 
-"""Example 6: IP Address Whitelisting
+"""
+Example : IP Address Whitelisting
 This middleware restricts access to the API by checking if the request is coming from a whitelisted IP address.
 This middleware checks the client’s IP address and blocks the request if the IP is not in the WHITELISTED_IPS set.
 """
